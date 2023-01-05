74d18016dfb367e4059ed3cc838ec9dcba0a2eb8
Dave Boster <dave@boster.dev>
2023-01-05 14:01:34 -0600

    Update Import-Pester.ps1
    Require a minimum version for Pester Import Script

--- internal/Import-Pester.ps1
+++ internal/Import-Pester.ps1
@@ -1,11 +1,12 @@
 $ModuleToImport = "Pester"
+$MinimumVersion = "5.3"
 
 Write-Debug "Importing $ModuleToImport Module"
-$powerShellModule = Get-Module -Name $ModuleToImport -ListAvailable
+$powerShellModule = Get-Module -Name $ModuleToImport -ListAvailable | Where-Object { $_.Version -ge $MinimumVersion }
 if (!$powerShellModule) {
     try {
-        Install-Module -Name $ModuleToImport -Scope CurrentUser -Force -SkipPublisherCheck
-        $powerShellModule = Get-Module -Name $ModuleToImport -ListAvailable
+        Install-Module -Name $ModuleToImport -Scope CurrentUser -MinimumVersion $MinimumVersion -Force -SkipPublisherCheck
+        $powerShellModule = Get-Module -Name $ModuleToImport -ListAvailable | Where-Object { $_.Version -ge $MinimumVersion }
     }
     catch {
         Write-Error "Failed to install the $ModuleToImport module" -ErrorAction Stop
